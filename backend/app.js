cb508fea0cdba997de09b62f016578ea61534480
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-02-03 22:27:14 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cors = require('cors');
+
+require("dotenv").config();
+//port
+const PORT = process.env.PORT || 9000
+
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+//database connection
+const connectDB = require("../backend/config/database");
+connectDB();
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(cors());
+
+
+// error middleware
+app.use(errorHandler);
+
+
+//routes middleware
+const authRoutes = require("./routes/authRoutes");
+app.use("/api",authRoutes);
+
+
+//server started
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+//default route
+app.get("/",(req,res) => {
+  res.send(`<h1>This is homepage baby.</h1>`)
+});
